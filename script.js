deb85acc805c801abb45dded6e0498afc8c845f6
Vidhi jain <117736922+vi3003@users.noreply.github.com>
2023-03-22 20:33:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let confirmation = confirm("Do you want to proceed?");
+
+if (confirmation) {
+  alert("You clicked OK. Proceeding...");
+} else {
+  alert("You clicked Cancel. Exiting...");
+}
